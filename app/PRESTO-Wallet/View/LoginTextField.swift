9e472690001042137b592aec61a5711928ddfcad
Jeffrey <jeffreyca16@gmail.com>
2017-12-23 23:28:01 -0500

    Add login screen layout

---
+++ app/PRESTO-Wallet/View/LoginTextField.swift
@@ -0,0 +1,76 @@
+//
+//  LoginTextField
+//  PRESTO-Wallet
+//
+//  Created by Jeffrey on 2017-12-23.
+//  Copyright © 2017 JeffreyCA. All rights reserved.
+//
+
+import UIKit
+
+class LoginTextField: UITextField {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable var placeHolderColor: UIColor? {
+        get {
+            return self.placeHolderColor
+        }
+        set {
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedStringKey.foregroundColor: newValue!])
+        }
+    }
+
+    @IBInspectable var paddingLeft: CGFloat {
+        get {
+            return leftView!.frame.size.width
+        }
+        set {
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: newValue, height: frame.size.height))
+            leftView = paddingView
+            leftViewMode = .always
+        }
+    }
+    
+    @IBInspectable var paddingRight: CGFloat {
+        get {
+            return rightView!.frame.size.width
+        }
+        set {
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: newValue, height: frame.size.height))
+            rightView = paddingView
+            rightViewMode = .unlessEditing
+        }
+    }
+    
+    override func clearButtonRect(forBounds bounds: CGRect) -> CGRect {
+        let originalRect = super.clearButtonRect(forBounds: bounds)
+        
+        return originalRect.offsetBy(dx: -10, dy: 0)
+    }
+}
