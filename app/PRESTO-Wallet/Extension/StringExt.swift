828bf51bbeec15711e1e09f4be589022192b3965
Jeffrey <jeffreyca16@gmail.com>
2017-12-25 23:08:26 -0500

    Add String extension and unit test

---
+++ app/PRESTO-Wallet/Extension/StringExt.swift
@@ -0,0 +1,23 @@
+//
+//  StringExt.swift
+//  PRESTO-Wallet
+//
+//  Created by Jeffrey on 2017-12-25.
+//  Copyright © 2017 JeffreyCA. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    public func isAlphanumeric() -> Bool {
+        return !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    }
+    
+    public func containsLetter() -> Bool {
+        return !isEmpty && range(of: "[a-zA-Z]", options: .regularExpression) != nil
+    }
+    
+    public func containsNumber() -> Bool {
+        return !isEmpty && range(of: "[0-9]", options: .regularExpression) != nil
+    }
+}
