a9320b47cfa4f62a3f48c9d38cb7e99792f69930
Jeffrey <jeffreyca16@gmail.com>
2017-12-30 22:42:29 -0500

    Create transaction model classes

---
+++ app/PRESTO-Wallet/Model/Transaction.swift
@@ -0,0 +1,18 @@
+//
+//  Transaction.swift
+//  PRESTO-Wallet
+//
+//  Created by Jeffrey on 2017-12-30.
+//  Copyright © 2017 JeffreyCA. All rights reserved.
+//
+
+import Foundation
+
+class Transaction {
+    var agency: TransitAgency?
+    var amount: Double?
+    var balance: Double?
+    var date: Date?
+    var discount: Double?
+    var location: String?
+}
---
+++ app/PRESTO-Wallet/Model/TransitAgency.swift
@@ -0,0 +1,22 @@
+//
+//  TransitAgency.swift
+//  PRESTO-Wallet
+//
+//  Created by Jeffrey on 2017-12-30.
+//  Copyright © 2017 JeffreyCA. All rights reserved.
+//
+
+enum TransitAgency {
+    case BRAMPTON
+    case BURLINGTON
+    case DURHAM
+    case GO
+    case HAMILTON
+    case MI_WAY
+    case OAKVILLE
+    case OC_TRANSPO
+    case PRESTO
+    case TTC
+    case UP_EXPRESS
+    case YRT_VIVA
+}
