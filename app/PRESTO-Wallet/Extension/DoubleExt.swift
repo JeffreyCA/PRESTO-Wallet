411b83dc214f3f66c79eb75dc7f362f1552f78cd
Jeffrey <jeffreyca16@gmail.com>
2017-12-30 22:42:57 -0500

    Add extension for Double

---
+++ app/PRESTO-Wallet/Extension/DoubleExt.swift
@@ -0,0 +1,19 @@
+//
+//  DoubleExt.swift
+//  PRESTO-Wallet
+//
+//  Created by Jeffrey on 2017-12-30.
+//  Copyright © 2017 JeffreyCA. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    var formattedAsCad: String {
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale(identifier: "en_CA")
+        return currencyFormatter.string(from: NSNumber(value: self))!
+    }
+}
