a6b76408bc872b156d7b5f1109d9fbff41fe64cc
Jeffrey <jeffreyca16@gmail.com>
2018-01-06 11:53:16 -0500

    Add view controller for filter dialog

---
+++ app/PRESTO-Wallet/Controller/Dashboard/FilterOptionsTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FilterOptionsTableViewController.swift
+//  PRESTO-Wallet
+//
+//  Created by Jeffrey on 2018-01-06.
+//  Copyright © 2018 JeffreyCA. All rights reserved.
+//
+
+import UIKit
+
+class FilterOptionsTableViewController: UITableViewController {
+
+    @IBAction func cancelDialog() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func finishDialog() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
