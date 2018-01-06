4bda7227ccfd4af22e30145ee4f83de2b09d9c84
Jeffrey <jeffreyca16@gmail.com>
2018-01-06 11:55:08 -0500

    Add SwiftMultiSelect to Pods

--- app/Pods/Target Support Files/Pods-PRESTO-Wallet/Pods-PRESTO-Wallet-frameworks.sh
+++ app/Pods/Target Support Files/Pods-PRESTO-Wallet/Pods-PRESTO-Wallet-frameworks.sh
@@ -107,6 +107,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MZAppearance/MZAppearance.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MZFormSheetPresentationController/MZFormSheetPresentationController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftMultiSelect/SwiftMultiSelect.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftSoup/SwiftSoup.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -115,6 +116,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MZAppearance/MZAppearance.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MZFormSheetPresentationController/MZFormSheetPresentationController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftMultiSelect/SwiftMultiSelect.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftSoup/SwiftSoup.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
